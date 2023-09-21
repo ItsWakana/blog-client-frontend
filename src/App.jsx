65d95dd4832c2f2f7b39ae31aa9b0d2d7a35feb1
itswakana <100365678+ItsWakana@users.noreply.github.com>
2023-09-22 00:17:24 +0100

    write post fetch for user log in

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react";
 
 function App() {
 
+  // "username": "jimmyjames2", "password": "jimmy167"
+  useEffect(() => {
+
+    // const getAuthToken = async () => {
+    //   const response = await fetch("http://localhost:3000/api/sign-in", {
+    //     method: 'POST',
+    //     headers: {
+    //       "Content-Type": 'application/json',
+    //     },
+    //     body: JSON.stringify({ username: "jimmyjames2", password: "jimmy167" })
+    //   });
+    //   const token = await response.json();
+    // }
+
+    //WORK ON FETCHING THE BLOG POSTS. WE SHOULD PROBABLY OPEN UP THE POSTS ROUTE TO ANYTHING SO WE DONT NEED ANY SIGN INS. THIS WAY USERS WHO ARENT LOGGED IN CAN STILL VIEW THE BLOG POSTS.
+    
+    //FOR NOW WE CAN LEAVE CORS OPEN, UNTIL THIS GOES INTO PRODUCTION.
+
+
+  },[])
   return <h1>Hello World!</h1>
 }
 
